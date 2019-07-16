b0a58e447fc5fe7cb25f72b02b63e4c670ccdd27
M Saif Ullah <chsaif369@gmail.com>
2019-07-17 02:38:26 +0500

    Sign in, register & Sign out User with Firebase Email/Password Authentication

--- app/src/main/java/com/saifi369/firebaseauthapp/MainActivity.java
+++ app/src/main/java/com/saifi369/firebaseauthapp/MainActivity.java
@@ -6,18 +6,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextInputLayout mEmailLayout, mPasswordLayout;
-    private Button mBtnSignin, mBtnRegisterUser;
+    private Button mBtnSignin, mBtnRegisterUser, mBtnSignoutUser;
     private TextView mOutputText;
     private ProgressBar mProgressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +36,19 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         initViews();
+        mAuth = FirebaseAuth.getInstance();
 
         mBtnSignin.setOnClickListener(this::singInUser);
         mBtnRegisterUser.setOnClickListener(this::createUser);
+        mBtnSignoutUser.setOnClickListener(this::signOutUser);
 
         hideProgressBar();
 
+    }
+
+    private void signOutUser(View view) {
+        mAuth.signOut();
+        updateUI();
     }
 
     private void singInUser(View view) {
@@ -43,6 +59,40 @@ public class MainActivity extends AppCompatActivity {
         }
         //Email and Password valid, sign in user here
 
+        String email = mEmailLayout.getEditText().getText().toString().trim();
+        String password = mPasswordLayout.getEditText().getText().toString().trim();
+        showProgressBar();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            hideProgressBar();
+                            Toast.makeText(MainActivity.this, "User logged in", Toast.LENGTH_SHORT).show();
+                            updateUI();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error occurred", Toast.LENGTH_SHORT).show();
+                            hideProgressBar();
+                        }
+
+                    }
+                });
+
+
+    }
+
+    private void updateUI() {
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user == null) {
+            mOutputText.setText("User not logged in");
+            return;
+        } else {
+            mOutputText.setText(user.getEmail());
+        }
     }
 
     private void createUser(View view) {
@@ -52,7 +102,30 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         //Email and Password valid, create user here
+        String email = mEmailLayout.getEditText().getText().toString().trim();
+        String password = mPasswordLayout.getEditText().getText().toString().trim();
+        showProgressBar();
 
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "User created", Toast.LENGTH_SHORT).show();
+                            hideProgressBar();
+                            updateUI();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error occurred", Toast.LENGTH_SHORT).show();
+                            hideProgressBar();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        updateUI();
 
     }
 
@@ -63,6 +136,7 @@ public class MainActivity extends AppCompatActivity {
         mBtnRegisterUser = findViewById(R.id.btn_registeruser);
         mOutputText = findViewById(R.id.tv_output);
         mProgressBar = findViewById(R.id.progressbar);
+        mBtnSignoutUser = findViewById(R.id.btn_signoutuser);
     }
 
     private boolean validateEmailAddress() {
@@ -90,7 +164,7 @@ public class MainActivity extends AppCompatActivity {
             return false;
         } else if (password.length() < 6) {
             mPasswordLayout.setError("Password short. Minimum 6 characters required.");
-            return true;
+            return false;
         } else {
             mPasswordLayout.setError(null);
             return true;
